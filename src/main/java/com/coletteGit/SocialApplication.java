d0fa9b60af0e9633f43c515627a9dbb7aa535176
Colette Turbeville <coletteturbeville@Colettes-MBP.hsd1.ca.comcast.net>
2020-04-17 11:33:31 -0700

    feat(core): oauth wip

--- src/main/java/com/coletteGit/SocialApplication.java
+++ src/main/java/com/coletteGit/SocialApplication.java
@@ -16,6 +16,8 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.json.JSONObject;
+
 @SpringBootApplication
 @RestController
 public class SocialApplication extends WebSecurityConfigurerAdapter {
@@ -23,11 +25,28 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
 
+        System.out.println(new JSONObject(principal.getAttributes()).toString());
+
+        // String token = cookies.get("io").value
+
+        /*
+         * # ... and POST it back to GitHub   result =
+         * RestClient.post('https://github.com/login/oauth/access_token',
+         *                           {:client_id => CLIENT_ID,
+         *                            :client_secret => CLIENT_SECRET,
+         *                            :code => session_code},
+         *                            :accept => :json)
+         */
+
         Map<String, Object> ret = new HashMap<String, Object>();
 
         ret.put("avatar_url", principal.getAttribute("avatar_url"));
 
+        ret.put("login", principal.getAttribute("login"));
+
         ret.put("bio", principal.getAttribute("bio"));
+
+        ret.put("repos_url", principal.getAttribute("repos_url"));
 
         return ret;
     }
